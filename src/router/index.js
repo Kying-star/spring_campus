53f783c35868f4fd35097d260d94e5f47e9fecd4
Cansiny0320 <1136843910@qq.com>
2020-12-22 15:45:41 +0800

    feat: 首页样式

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,23 @@
 import { createRouter, createWebHashHistory } from "vue-router";
+import NProgress from "nprogress";
 
-const routes = [];
+const routes = [
+  {
+    path: "/",
+    component: () => import("@views/Home.vue")
+  }
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
 });
 
+router.beforeEach(() => {
+  NProgress.start();
+});
+
+router.afterEach(() => {
+  NProgress.done();
+});
 export default router;
