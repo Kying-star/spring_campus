470ca4bb040020aa0f60c9cf3823438a9d2821e1
Cansiny0320 <1136843910@qq.com>
2020-12-28 21:48:46 +0800

    fix: remove vconsole

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./assets/css/reset.css";
-import Vconsole from "vconsole";
-process.env.NODE_ENV === "development" && new Vconsole();
 createApp(App)
   .use(store)
   .use(router)
