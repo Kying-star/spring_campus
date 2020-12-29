e69ce564889bb8c009eb8705963fe19eb742c150
kying-star <3077006505@qq.com>
2020-12-30 01:57:07 +0800

    fix

--- src/views/RollList.vue
+++ src/views/RollList.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-12-26 15:47:00
- * @LastEditTime: 2020-12-30 00:52:12
+ * @LastEditTime: 2020-12-30 01:56:56
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /the-19th-committee/src/views/RollList.vue
@@ -89,6 +89,9 @@ export default {
     const showList = index => {
       // console.log(index);
       type_index.value = index === -1 ? 0 : index;
+      if (type_index.value > 0) {
+        return;
+      }
       // 写的跟屎一样的刷新，回来再改
       fetchRank(type_index.value);
     };
