1d68399f4b0d486faaf717576b63119ac7e44a04
kying-star <3077006505@qq.com>
2020-12-29 21:24:51 +0800

    feat:压缩图片

--- src/components/Choice.vue
+++ src/components/Choice.vue
@@ -14,7 +14,7 @@
       <div class="answers">
         <div
           :class="getClass(item.answer)"
-          v-for="(item) of answersForSelect"
+          v-for="item of answersForSelect"
           :key="item.index"
           @click="select(item.answer)"
         >
