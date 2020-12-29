7cf3ebcdb0ec742c725654f989e3b964b53d281b
Cansiny0320 <1136843910@qq.com>
2020-12-30 00:39:38 +0800

    fix: add vconsole

--- src/main.js
+++ src/main.js
@@ -11,8 +11,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./assets/css/reset.css";
-// import Vconsole from "vconsole";
-// new Vconsole();
+import Vconsole from "vconsole";
+new Vconsole();
 createApp(App)
   .use(store)
   .use(router)
