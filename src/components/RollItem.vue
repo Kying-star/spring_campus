150a2d74683d29e2c33fecf219c6d9d21acf1d32
kying-star <3077006505@qq.com>
2020-12-30 09:57:18 +0800

    fix

--- src/components/RollItem.vue
+++ src/components/RollItem.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-12-26 23:04:50
- * @LastEditTime: 2020-12-30 09:54:37
+ * @LastEditTime: 2020-12-30 09:57:09
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /the-19th-committee/src/components/RollItem.vue
@@ -123,7 +123,7 @@ export default {
     line-height: 38px;
   }
   .times {
-    width: 85px;
+    width: 160px;
     display: flex;
     align-content: flex-start;
   }
