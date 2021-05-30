ca869d8ce9fff204a2812d736f6b0be94f063e51
myjdml <898762316@qq.com>
2021-05-30 20:50:51 +0800

    feat: 新增历史卡片界面

---
+++ src/components/HistoryCardHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="history-card-header">
+
+  </div>
+</template>
+
+<script>
+// import { ref } from "vue";
+
+export default {
+  name: "HistoryCardHeader",
+  setup () {
+    return {
+
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.history-card-header {
+  height: 80px;
+  background: url(~@assets/images/HistoryCard/rope.png);
+}
+</style>
--- src/views/HistoryCard.vue
+++ src/views/HistoryCard.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="history-card">
-
+    <history-card-header></history-card-header>
   </div>
 </template>
 
 <script>
+// import { ref } from "vue";
+import HistoryCardHeader from "@/components/HistoryCardHeader";
+
 export default {
-  name: "HistoryCard"
+  name: "HistoryCard",
+  components: { HistoryCardHeader },
+  setup () {
+    return {
+
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 .history-card {
+  height: 100vh;
+  background: url(~@assets/images/base/inner-bk.png);
+  background-size: 100%;
+  background-position: center bottom;
+  background-repeat: no-repeat;
 }
 </style>
